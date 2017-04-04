376c2338bad1b5fdf10fd04ec21362d238563c8b
D. Wilcox <dw@angk.org>
2017-04-03 22:04:10 -0500

    Changed name of socket file.

---
+++ server.py
@@ -0,0 +1,18 @@
+import tornado.web
+from tornado.ioloop import IOLoop
+from terminado import TermSocket, SingleTermManager
+
+if __name__ == '__main__':
+    term_manager = SingleTermManager(shell_command=['bash'])
+    handlers = [
+        (r"/ws", TermSocket, {'term_manager': term_manager}),
+    ]
+    app = tornado.web.Application(handlers)
+    app.listen(80)
+    try:
+        IOLoop.current().start()
+    except KeyboardInterrupt:
+        pass
+    finally:
+        term_manager.shutdown()
+        print("\n")
